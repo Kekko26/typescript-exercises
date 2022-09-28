6a7810c4acd7a48544855b15750c58adb46bd9dc
Francesco Pio Acciarino <Kekko26@outlook.com>
2022-09-28 13:01:46 +0200

    Exercise done

---
+++ Arrays/exercise-3-tuple-types-start.js
@@ -0,0 +1,9 @@
+/**
+ * START: Follow the instructions below.
+ */
+const countryPopulation = ["China", 1412600000];
+const countryCurrency = ["Italy", "Euro", "EUR"];
+const countryLanguage = ["Greece", "Greek"];
+// countryLanguage.reverse();
+console.log(countryLanguage);
+export {};
